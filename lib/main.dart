ed07b69b2005c0cd4a70207eceb2e77f084d3ff9
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-02-15 18:07:34 -0300

    use theme provider to alternate themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // @dart=2.10
 import 'dart:io';
 
+import 'package:auto_aula/providers/theme_provider.dart';
 import 'package:auto_aula/screens/home.dart';
 import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
@@ -13,24 +14,19 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final dir = join(Platform.environment['AppData'] ?? '', 'auto_aula');
   Hive.init(dir);
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          darkTheme: ThemeData(
-            primarySwatch: Colors.cyan,
-            brightness: Brightness.dark,
-          ),
-          home: Home()),
+  // ignore: type_annotate_public_apis
+  Widget build(BuildContext context, watch) {
+    final theme = watch(themeProvider.state);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: theme,
+      home: Home(),
     );
   }
 }
---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final _lightTheme = ThemeData(
+  primarySwatch: Colors.blue,
+);
+
+final _darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primarySwatch: Colors.blue,
+);
+
+final themeProvider = StateNotifierProvider((_) => ThemeNotifier());
+
+class ThemeNotifier extends StateNotifier<ThemeData> {
+  ThemeNotifier() : super(_lightTheme);
+  void useLightTheme() {
+    if (state == _lightTheme) return;
+    state = _lightTheme;
+  }
+
+  void useDarkTheme() {
+    if (state == _darkTheme) return;
+    state = _darkTheme;
+  }
+}

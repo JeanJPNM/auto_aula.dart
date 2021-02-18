38815b8a14e3441336c0b8ad22a3c773324b0043
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-02-18 08:53:09 -0300

    use a more debug friendly way to get themes

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -2,26 +2,26 @@ import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final _lightTheme = ThemeData(
-  primarySwatch: Colors.blue,
-);
+ThemeData get _lightTheme => ThemeData(
+      primarySwatch: Colors.indigo,
+    );
 
-final _darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  primarySwatch: Colors.cyan,
-);
+ThemeData get _darkTheme => ThemeData(
+      brightness: Brightness.dark,
+      primarySwatch: Colors.cyan,
+    );
 
 final themeProvider = StateNotifierProvider((_) => ThemeNotifier());
 
 class ThemeNotifier extends StateNotifier<ThemeData> {
   ThemeNotifier() : super(_lightTheme);
   void useLightTheme() {
-    if (state == _lightTheme) return;
+    if (state.brightness == Brightness.light) return;
     state = _lightTheme;
   }
 
   void useDarkTheme() {
-    if (state == _darkTheme) return;
+    if (state.brightness == Brightness.dark) return;
     state = _darkTheme;
   }
 }

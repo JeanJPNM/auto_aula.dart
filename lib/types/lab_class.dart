b76c5fd3f9a928cae56f886a9397a70357a19866
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-02-22 18:17:06 -0300

    remove old app

--- lib/types/lab_class.dart
+++
@@ -1,1 +0,0 @@
-enum LabClass { bio, info }
--- lib/types/online_class.dart
+++
@@ -1,8 +0,0 @@
-class OnlineClass {
-  OnlineClass(int hour, int minute) {
-    final now = DateTime.now();
-    start = DateTime(now.year, now.month, now.day, hour, minute);
-    end = start.add(const Duration(minutes: 50));
-  }
-  late DateTime start, end;
-}

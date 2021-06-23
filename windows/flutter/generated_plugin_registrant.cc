b37d976e7b0e1dc1cea471895aef0c36b5bd0dbc
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2021-06-23 12:54:06 -0300

    install desktoast

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -4,6 +4,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <desktoasts/desktoasts_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DesktoastsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DesktoastsPlugin"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  desktoasts
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
